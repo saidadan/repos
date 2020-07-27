e93882e9c4ad739d46d9ed8b9d105ffa42fa3ccf
saidadan <saidadan05@gmail.com>
2020-07-28 02:18:08 +0300

    Thirdcommit

--- hungry.py
+++ hungry.py
@@ -2,8 +2,5 @@ hungry=input("are you hungry?")
 if hungry == "yes":
 	print("eat samosa")
 	print("eat pizza")
-	print("drink juice")
-	print("lala")
-	print("drink tea")
 else:
 	print("do your homework")
